ae702e50aa705e58b069aee9b1a4c361d81086c7
calezich <nicolas.calezich@accenture.com>
2021-11-10 23:52:30 -0300

    addUser validations added

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,8 +1,13 @@
 const userService = require('../services/users.service')
 
 const createUser = async (req, res) => {
-    const result = await userService.createUser(req.body)
-    res.send(result)
+    try{
+        const result = await userService.createUser(req.body)
+        res.send(result)
+    }
+    catch (error){
+        res.status(error.status).send(error.message)
+    }
 }
 
 module.exports = { createUser }
--- data/users.js
+++ data/users.js
@@ -5,14 +5,31 @@ const bcrypt = require('bcryptjs');
 
 async function createUser(user){
     const connectiondb = await connection.getConnection();
+    if (!user.username ||
+        !user.password ||
+        !user.dni
+        ){
+            throw({message: "Username, Password and Dni are required", status: 400});
+        }
+    else{
+        user.password = await bcrypt.hash(user.password, 8);
+        const result = await connectiondb.db('api-db')
+                            .collection('users')
+                            .insertOne(user);
+        return result;
+    }
+}
 
-    // los nombres de base de datos pueden cambiar   
-    user.password = await bcrypt.hash(user.password, 8);
+async function findByDni(dni){
+    const connectiondb = await connection.getConnection();
+    const user = await connectiondb.db('sample_betp2')
+                        .collection('usuarios')
+                        .findOne({dni: dni});
+    if(!user){
+        throw new Error('Credenciales no validas');
+    }
 
-    const result = await connectiondb.db('api-db')
-                        .collection('users')
-                        .insertOne(user);
-    return result;
+    return user;
 }
 
 module.exports = {createUser};
