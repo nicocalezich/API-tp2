0db66d7bbcc5eb74e7cdd84edd0554889f6a5955
calezich <nicolas.calezich@accenture.com>
2021-11-13 11:01:14 -0300

    create User fixed

--- data/users.data.js
+++ data/users.data.js
@@ -5,9 +5,8 @@ const bcrypt = require('bcryptjs');
 
 async function createUser(user){
     const connectiondb = await connection.getConnection();
-    console.log(findByDni(user.dni),findByDni(user.dni) == null)
     if (!user.username || !user.password || !user.dni) throw({message: "Username, Password and Dni are required", status: 400})
-    if (findByDni(user.dni) != null) throw({message: "User already exists", status: 400})
+    if (await findByDni(user.dni)) throw({message: "User already exists", status: 400})
     if (user.username.length < 5 || user.username.length > 15) throw({message: "Invalid username, min 5 characters and max 15 characters", status: 400})
     if (user.password.length < 6) throw({message: "Invalid password, must have at least 6 characters", status: 400})
     if (user.dni.length < 7) throw({message: "Invalid dni, must have at least 7 characters", status: 400})
@@ -24,7 +23,9 @@ async function findByDni(dni){
     const searchedUser = await connectiondb.db('api-db')
                         .collection('users')
                         .findOne({dni: dni});
-    // console.log("Usuario encontrado ->", searchedUser)
+    // if (!searchedUser){
+    //     throw({message: "User not found", status: 404})
+    // }
     return searchedUser;
 }
 
