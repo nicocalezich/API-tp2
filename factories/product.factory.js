8bde27bc0c575c9c7b81675af8d9b9c3db51d2ae
Tomas Perez <perezvitale.tomas@gmail.com>
2021-11-17 20:12:03 -0300

    Se comienza desarrollo de factory para Productos

---
+++ factories/product.factory.js
@@ -0,0 +1,8 @@
+const Product = require('../models/product.model')
+
+module.exports = 
+{
+    create: (product) =>{
+        return new Product(product.id,product.name,product.stock,product.type,product.price);
+    }
+}
---
+++ models/product.model.js
@@ -0,0 +1,9 @@
+export class Product{
+    constructor(id,name,stock, type, price){
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+        this.type = type;
+        this.price = price;
+    }
+}
