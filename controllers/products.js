f6057e8017eece5d15195a352e6428bcd4d9fc0d
Tomas Perez <perezvitale.tomas@gmail.com>
2021-11-17 20:57:06 -0300

    Commit provisorio.

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,14 @@
 const productService = require('../services/product.service')
 
+
 const insertProduct = async (req, res) => {
-    const result = await productService.insertProduct(req.body)
-    res.send(result)
+    try {
+        const result = await productService.insertProduct(req.body);
+        res.send(result);    
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 module.exports = { insertProduct }
