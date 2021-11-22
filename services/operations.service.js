2f78d7616b1b2705c44ceb9826bd91b740e1bcf0
calezich <nicolas.calezich@accenture.com>
2021-11-22 11:13:06 -0300

    feat calculateTotal fixed

--- services/operations.service.js
+++ services/operations.service.js
@@ -2,27 +2,25 @@ const operationsRepository = require('../data/operations.data')
 const salesFactory = require('../factories/sale.factory')
 const productService = require('../services/products.services')
 
+const newSale = async (sale) => {
+  sale.total = await calculateTotal(sale.products)
+  sale.date = new Date()
+  const newSale = salesFactory.create(sale)
+  const result = await operationsRepository.insertNewSale(newSale)
+  return result
+}
 
-    const newSale = async (sale) => {
-        sale.total = calculateTotal(sale.products)
-        sale.date = new Date()
-        const newSale = salesFactory.create(sale)
-        const result = await operationsRepository.insertNewSale(newSale)
-        return result
+const calculateTotal = async (products) => {
+  let total = 0
+  for (let i = 0; i < products.length; i++) {
+    let product = await productService.getProduct(products[i].id)
+    if (!product) {
+      throw new Error
+    }
+    total += (product.price * products[i].qty)
   }
-// resolver suma de total async await
-  const calculateTotal =  (products) => {
-      let total = 0
-      products.forEach( (item) => {
-        productService.getProduct(item.id)
-        .then((product) => {
-            total += (product.price * item.qty)
-            console.log(total)
-        })
-      })
-     
-      
-  }
+  return total
+}
 
 module.exports = { newSale }
 
